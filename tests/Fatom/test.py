9e967af16130a0d76183ba85560473e22377b582
Jean-Luc Fattebert <fattebertj@ornl.gov>
2019-04-23 12:03:03 -0400

    Add regression test: F atom

---
+++ tests/Fatom/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import sys
+import os
+import subprocess
+import string
+
+print("Test Fluorine atom...")
+
+mpicmd = sys.argv[1]+" "+sys.argv[2]+" "+sys.argv[3]
+exe = sys.argv[4]
+inp = sys.argv[5]
+coords = sys.argv[6]
+print("coordinates file: %s"%coords)
+
+#create links to potentials files
+dst = 'pseudo.F_ONCV_PBE_SG15'
+src = sys.argv[7] + '/' + dst
+
+if not os.path.exists(dst):
+  print("Create link to %s"%dst)
+  os.symlink(src, dst)
+
+#run mgmol
+command = "{} {} -c {} -i {}".format(mpicmd,exe,inp,coords)
+output = subprocess.check_output(command,shell=True)
+
+#analyse mgmol standard output
+#make sure force is below tolerance
+lines=output.split(b'\n')
+
+print("Check force...")
+tol = 1.e-2
+for line in lines:
+  num_matches = line.count(b'%%')
+  if num_matches:
+    print(line)
+  num_matches = line.count(b'##')
+  if num_matches:
+    words=line.split()
+    if len(words)==8:
+      print(line)
+      for i in range(5,8):
+        if abs(eval(words[i]))>tol:
+          sys.exit(1)
+
+print("Check eigenvalues...")
+tole=1.e-3
+for i in range(len(lines)):
+  num_matches1 = lines[i].count(b'convergence')
+  num_matches2 = lines[i].count(b'achieved')
+  if num_matches1 and num_matches2:
+    ii=i+3
+    eigenvalues=lines[ii].split()
+    print("Eigenvalues: {}".format(eigenvalues) )
+    if abs(eval(eigenvalues[0])+1.070)>tole:
+      print("ERROR Eigenvalue 0 = {}".format(eval(eigenvalues[0])))
+      sys.exit(1)
+    for ii in range(3):
+      if abs(eval(eigenvalues[1+ii])+0.409)>tole:
+        print("ERROR Eigenvalue {} = {}".format(1+ii,eval(eigenvalues[1+ii])))
+        sys.exit(1)
+    sys.exit(0)
+
