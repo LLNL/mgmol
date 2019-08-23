d7152b0e39017e76d8681d5160c6521f0e96f2f8
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-08-23 15:50:55 -0400

    Code cov (#24)
    * Add lcov to Docker image
    
    * Add make coverage target to build system
    
    * Add codecov to ci

--- ci/compile_and_run.sh
+++ ci/compile_and_run.sh
@@ -6,10 +6,17 @@ mkdir build && cd build
 ARGS=(
   -D SCALAPACK_ROOT=/usr/lib/x86_64-linux-gnu
   -D MGMOL_WITH_CLANG_FORMAT=ON
+  -D MGMOL_WITH_COVERAGE=ON
   )
 cmake "${ARGS[@]}" ../
 make -j8
 ctest --no-compress-output -T Test
 make format && git diff --exit-code
+
+# Code coverage
+make coverage
+curl -s https://codecov.io/bash -o codecov_bash_uploader
+chmod +x codecov_bash_uploader
+./codecov_bash_uploader -Z -X gcov -f lcov.info
 
 exit 0
