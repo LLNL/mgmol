1eceb21b80d369859f955bb9ac9de2981e860e7c
Jean-Luc Fattebert <fattebertj@ornl.gov>
2019-09-11 11:58:14 -0400

    Fix build scripts to use gcc on Summit @ OLCF

--- scripts/build_summit.sh
+++ scripts/build_summit.sh
@@ -2,28 +2,16 @@
 
 # load some modules
 source scripts/modules.summit
-#module load cmake/3.8.2
-#module load xl/2018.11.26
-#module load essl
-#module load lapack/3.8.0-gcc-4.9.3
-#module load hdf5-parallel
 
 # set some environment variables. Set them explicitly or use loaded module path (preferred)
-#
-# set SCALAPACK_ROOT to automatically attempt to find scalapack libraries and headers. This is 
-# useful if the libraries/ headers have well-defined names like scalapack.a(.so) and scalapack.h.
-# Otherwise provide paths for SCALAPACK_LIBRARY and DSCALAPACK_INCLUDE_DIR (as below). 
-#setenv SCALAPACK_ROOT ${LAPACK_DIR}
 
 # hdf5
 setenv HDF5_ROOT ${OLCF_HDF5_ROOT}
 
-# We need to define the cmake blas vendor option here to find the right one.
-set BLAS_VENDOR = IBMESSL
+setenv SCALAPACK_ROOT ${OLCF_NETLIB_SCALAPACK_ROOT}
 
-# manually set the location of BLACS libraries for scalapack
-#set BLACS_LIB = ${SCALAPACK_ROOT}/lib
-#set BLACS_LIB = ${MKLROOT}/lib/intel64/
+# We need to define the cmake blas vendor option here to find the right one.
+set BLA_VENDOR = IBMESSL
 
 set MGMOL_ROOT = `pwd`
 
@@ -34,20 +22,15 @@ set BUILD_DIR = ${MGMOL_ROOT}/build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-# call cmake 
-#      -DSCALAPACK_LIBRARY=/usr/tcetmp/packages/scalapack/scalapack-2.0.2-gfortran-4.8.5/lib/libscalapack.a \
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
       -DCMAKE_CXX_COMPILER=mpicxx \
       -DMPIEXEC_EXECUTABLE="/sw/summit/xalt/1.1.3/bin/jsrun" \
       -DMPIEXEC_NUMPROCS_FLAG="-n" \
       -DMPIEXEC_PREFLAGS="-a1;-c7;-bpacked:2;-g1" \
-      -DBLA_VENDOR=${BLAS_VENDOR} \
+      -DBLA_VENDOR=${BLA_VENDOR} \
       -DLAPACK_LIBRARIES=${OLCF_ESSL_ROOT}/lib64/libesslsmp.so \
-      -DSCALAPACK_LIBRARY=${OLCF_NETLIB_SCALAPACK_ROOT}/lib/libscalapack.so \
-#      -DSCALAPACK_INCLUDE_DIR=${LAPACK_DIR} \
       ..
 
 # call make install
-#make
 make -j
 make install
