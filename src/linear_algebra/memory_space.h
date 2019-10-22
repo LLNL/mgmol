85d65470f35c054c579a8f4efe155bd2c2d8f275
Bruno Turcksin <bruno.turcksin@gmail.com>
2019-10-22 19:49:30 +0000

    Add more functions to copy between host and device

--- src/linear_algebra/memory_space.h
+++ src/linear_algebra/memory_space.h
@@ -32,58 +32,106 @@ struct Device
 
 #ifdef HAVE_MAGMA
 template <typename T>
-void copy_to_dev(std::vector<T> const& vec, T* vec_dev)
+void copy_to_dev(T const* const vec, unsigned int size, T* vec_dev)
 {
+    int const increment   = 1;
     auto& magma_singleton = MagmaSingleton::get_magma_singleton();
-    magma_setvector(vec.size(), sizeof(T), vec.data(), 1, vec_dev, 1,
+    magma_setvector(size, sizeof(T), vec, increment, vec_dev, increment,
         magma_singleton.queue);
 }
 #else
+
 template <typename T>
-void copy_to_dev(std::vector<T> const&, T*)
+void copy_to_dev(T* const, unsigned int, T*)
 {
+    // TODO
 }
 #endif
+
+template <typename T>
+void copy_to_dev(std::vector<T> const& vec, T* vec_dev)
+{
+    copy_to_dev(vec.data(), vec.size(), vec_dev);
+}
 
 template <typename T>
 void copy_to_dev(
     std::vector<T> const& vec, std::unique_ptr<T[], void (*)(T*)>& vec_dev)
 {
-    copy_to_dev(vec, vec_dev.get());
+    copy_to_dev(vec.data(), vec.size(), vec_dev.get());
 }
 
 template <typename T>
 void copy_to_dev(std::vector<T> const& vec, std::shared_ptr<T[]>& vec_dev)
 {
-    copy_to_dev(vec, vec_dev.get());
+    copy_to_dev(vec, vec.size, vec_dev.get());
+}
+
+template <typename T>
+void copy_to_dev(T const* const vec, unsigned int size,
+    std::unique_ptr<T[], void (*)(T*)>& vec_dev)
+{
+    copy_to_dev(vec, size, vec_dev.get());
 }
 
 #ifdef HAVE_MAGMA
 template <typename T>
-void copy_to_host(T const* const vec_dev, std::vector<T>& vec)
+void copy_to_host(T const* const vec_dev, unsigned int size, T* vec)
 {
+    int const increment   = 1;
     auto& magma_singleton = MagmaSingleton::get_magma_singleton();
-    magma_getvector(vec.size(), sizeof(T), vec_dev, 1, vec.data(), 1,
+    magma_getvector(size, sizeof(T), vec_dev, increment, vec, increment,
         magma_singleton.queue);
 }
 #else
 template <typename T>
-void copy_to_host(T const* const, std::vector<T>&)
+void copy_to_host(T const* const vec_dev, unsigned int size, T* vec)
 {
+    // TODO
 }
 #endif
+
+template <typename T>
+void copy_to_host(T const* const vec_dev, std::vector<T>& vec)
+{
+    copy_to_host(vec_dev, vec.size(), vec.data());
+}
 
 template <typename T>
 void copy_to_host(
     std::unique_ptr<T[], void (*)(T*)> const& vec_dev, std::vector<T>& vec)
 {
-    copy_to_host(vec_dev.get(), vec);
+    copy_to_host(vec_dev.get(), vec.size(), vec.data());
 }
 
 template <typename T>
 void copy_to_host(std::shared_ptr<T[]> const& vec_dev, std::vector<T>& vec)
 {
-    copy_to_host(vec_dev.get(), vec);
+    copy_to_host(vec_dev.get(), vec.size(), vec.data());
 }
+
+template <typename T>
+void copy_to_host(std::unique_ptr<T[], void (*)(T*)> const& vec_dev,
+    unsigned int size, T* vec)
+{
+    copy_to_host(vec_dev.get(), size, vec);
+}
+
+#ifdef HAVE_MAGMA
+template <typename T>
+T* allocate_data_dev(unsigned int size)
+{
+    T* ptr_dev;
+    magma_malloc(reinterpret_cast<void**>(&ptr_dev), size * sizeof(T));
+
+    return ptr_dev;
+}
+
+template <typename T>
+void delete_data_dev(T* ptr_dev)
+{
+    magma_free(ptr_dev);
+}
+#endif
 }
 #endif
