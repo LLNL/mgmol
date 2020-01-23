ee8144c40f3d730de785344c38b3153e126f4374
Jean-Luc Fattebert <fattebertj@ornl.gov>
2020-01-23 17:15:44 -0500

    Threaded test for VariableSizeMatrix insert function (#78)
    * Add test for inserting elements in threaded case
    
    * Make a seperate test for gemv

--- tests/testVariableSizeMatrix.cc
+++ tests/testVariableSizeMatrix.cc
@@ -20,7 +20,69 @@
 #include <iostream>
 #include <numeric>
 
-namespace tt = boost::test_tools;
+namespace tt  = boost::test_tools;
+namespace utf = boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(variable_size_matrix_gemv, *utf::tolerance(1.e-8))
+{
+    const int lsize = 15;
+
+    VariableSizeMatrix<sparserow> mat("mat", lsize);
+    std::vector<int> rows;
+    for (int i = 0; i < lsize; i++)
+    {
+        rows.push_back(i);
+    }
+
+    mat.setupSparseRows(rows);
+
+    mat.set2Identity();
+
+    // gemv
+    std::cout << "Check gemv ... " << std::endl;
+    std::vector<double> x(lsize, 1.0);
+    std::vector<double> y(lsize, 0.);
+    mat.gemv(1.0, x, 0., y);
+    double sum = std::accumulate(y.begin(), y.end(), 0.);
+    BOOST_TEST(sum == (double)(lsize));
+}
+
+BOOST_AUTO_TEST_CASE(variable_size_matrix_insert, *utf::tolerance(1.e-12))
+{
+    const int lsize = 15;
+
+    // start with a matrix with only half of the rows we need
+    VariableSizeMatrix<sparserow> mat("A", lsize / 2);
+
+#ifdef _OPENMP
+    int numthreads = omp_get_max_threads();
+#else
+    int numthreads = 1;
+#endif
+    std::cout << "Number of threads = " << numthreads << std::endl;
+
+    // repeat insertions 10 times
+    for (int n = 0; n < 10; n++)
+    {
+
+#pragma omp parallel for
+        for (int j = 0; j < lsize; j++)
+        {
+            for (int i = 0; i < lsize; i++)
+                mat.insertMatrixElement(i, j, 1., ADD, true);
+        }
+    }
+
+    // verify values in matrix
+    for (int i = 0; i < lsize; i++)
+    {
+        for (int j = 0; j < lsize; j++)
+        {
+            double val = mat.get_value(i, j);
+            BOOST_TEST(val == 10.);
+        }
+    }
+}
 
 BOOST_AUTO_TEST_CASE(variable_size_matrix)
 {
@@ -107,15 +169,7 @@ BOOST_AUTO_TEST_CASE(variable_size_matrix)
     BOOST_TEST(sum == 0.0, tt::tolerance(1.e-8));
 
     // set2Identity
-    std::cout << "set to identity ..." << std::endl;
     matB.set2Identity();
-    // gemv
-    std::cout << "Check gemv ... " << std::endl;
-    std::vector<double> x((lsize + 1), 1.0);
-    std::vector<double> y((lsize + 1), 0.);
-    matB.gemv(1.0, x, 0., y);
-    sum = std::accumulate(y.begin(), y.end(), 0.);
-    BOOST_TEST(sum == (double)(lsize + 1), tt::tolerance(1.e-8));
 
     // scale
     double scal = 2.0;
