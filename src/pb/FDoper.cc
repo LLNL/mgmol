39390a875bf6f1ce929145a3019c36a32f659e1a
Wenjun Ge <39968398+wjge@users.noreply.github.com>
2020-07-15 09:16:04 -0400

    Add OMP OFFLOAD to the del2_4th finite difference scheme (#113)

--- src/pb/FDoper.cc
+++ src/pb/FDoper.cc
@@ -12,6 +12,15 @@
 #include "tools.h"
 #include <iomanip>
 
+#include "memory_space.h"
+
+#ifdef HAVE_MAGMA
+#include "magma_v2.h"
+
+template <typename ScalarType>
+using MemoryDev = MemorySpace::Memory<ScalarType, MemorySpace::Device>;
+#endif
+
 namespace pb
 {
 
@@ -397,45 +406,62 @@ void FDoper<T>::del2_4th(GridFunc<T>& A, GridFunc<T>& B) const
     const int dim1 = A.dim(1);
     const int dim2 = A.dim(2);
 
+#ifdef HAVE_OPENMP_OFFLOAD
+    const size_t ng = grid_.sizeg();
+    std::unique_ptr<T, void (*)(T*)> A_uu_dev(
+        MemoryDev<T>::allocate(ng), MemoryDev<T>::free);
+    MemorySpace::copy_to_dev(A.uu(0), ng, A_uu_dev.get());
+
+    std::unique_ptr<T, void (*)(T*)> B_uu_dev(
+        MemoryDev<T>::allocate(ng), MemoryDev<T>::free);
+
+    T* const A_uu_alias = A_uu_dev.get();
+    T* B_uu_alias       = B_uu_dev.get();
+#else
+    T* const A_uu_alias = A.uu(0);
+    T* B_uu_alias       = B.uu(0);
+#endif
+
+    int incx = incx_;
+    int incy = incy_;
+
+    MGMOL_PARALLEL_FOR_COLLAPSE(3, A_uu_alias, B_uu_alias)
     for (int ix = 0; ix < dim0; ix++)
     {
-        int iiy = iix + gpt * incy_;
-
         for (int iy = 0; iy < dim1; iy++)
         {
-            int iiz = iiy + gpt;
-
-            const T* __restrict__ v0   = A.uu(iiz);
-            const T* __restrict__ vmx  = A.uu(iiz - incx_);
-            const T* __restrict__ vpx  = A.uu(iiz + incx_);
-            const T* __restrict__ vmx2 = A.uu(iiz - incx2);
-            const T* __restrict__ vpx2 = A.uu(iiz + incx2);
-            const T* __restrict__ vmy  = A.uu(iiz - incy_);
-            const T* __restrict__ vpy  = A.uu(iiz + incy_);
-            const T* __restrict__ vmy2 = A.uu(iiz - incy2);
-            const T* __restrict__ vpy2 = A.uu(iiz + incy2);
-
-            T* __restrict__ u = B.uu(iiz);
             for (int iz = 0; iz < dim2; iz++)
             {
-                u[iz] = (c0 * (double)v0[iz]
+                int iiz = (iix + ix * incx + gpt * incy + iy * incy) + gpt;
 
-                         + c1x * ((double)vmx[iz] + (double)vpx[iz])
-                         + c1y * ((double)vmy[iz] + (double)vpy[iz])
-                         + c1z * ((double)v0[iz - 1] + (double)v0[iz + 1])
+                const T* __restrict__ v0   = A_uu_alias + iiz;
+                const T* __restrict__ vmx  = A_uu_alias + (iiz - incx);
+                const T* __restrict__ vpx  = A_uu_alias + (iiz + incx);
+                const T* __restrict__ vmx2 = A_uu_alias + (iiz - incx2);
+                const T* __restrict__ vpx2 = A_uu_alias + (iiz + incx2);
+                const T* __restrict__ vmy  = A_uu_alias + (iiz - incy);
+                const T* __restrict__ vpy  = A_uu_alias + (iiz + incy);
+                const T* __restrict__ vmy2 = A_uu_alias + (iiz - incy2);
+                const T* __restrict__ vpy2 = A_uu_alias + (iiz + incy2);
 
-                         + c2x * ((double)vmx2[iz] + (double)vpx2[iz])
-                         + c2y * ((double)vmy2[iz] + (double)vpy2[iz])
-                         + c2z * ((double)v0[iz - 2] + (double)v0[iz + 2]));
+                T* __restrict__ u = B_uu_alias + iiz;
 
-                iiz++;
+                u[iz] = c0 * (double)v0[iz]
+
+                        + c1x * ((double)vmx[iz] + (double)vpx[iz])
+                        + c1y * ((double)vmy[iz] + (double)vpy[iz])
+                        + c1z * ((double)v0[iz - 1] + (double)v0[iz + 1])
+
+                        + c2x * ((double)vmx2[iz] + (double)vpx2[iz])
+                        + c2y * ((double)vmy2[iz] + (double)vpy2[iz])
+                        + c2z * ((double)v0[iz - 2] + (double)v0[iz + 2]);
             }
-
-            iiy += incy_;
         }
-
-        iix += incx_;
     }
+
+#ifdef HAVE_OPENMP_OFFLOAD
+    MemorySpace::copy_to_host(B_uu_alias, ng, B.uu(0));
+#endif
 
     B.set_updated_boundaries(0);
 
@@ -725,7 +751,7 @@ void FDoper<T>::del2_4th_Mehr(GridFunc<T>& A, GridFunc<T>& B) const
                    c0mehr4_,czmehr4_,cymehr4_,cxmehr4_,cyzmehr4_,cxymehr4_,cxzmehr4_,
                    A.uu(0),B.uu(0));
 #else
-    const int iix0 = shift * incx_;
+    const int iix0      = shift * incx_;
 
     const T* const v = A.uu(0);
     T* u             = B.uu(0);
