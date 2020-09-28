2d71f3d5c71835193d49aaa8b034643b7abd9655
Jean-Luc Fattebert <fattebertj@ornl.gov>
2020-09-28 15:02:18 -0400

    Fix format

--- tests/testReplicatedMatrix.cc
+++ tests/testReplicatedMatrix.cc
@@ -14,34 +14,39 @@ TEST_CASE("Check ReplicatedMatrix", "[replicated_matrix]")
 
     // diagonalize diagonal matrix
     {
-    std::vector<double> diag(n);
-    for(int i=0;i<n;i++)diag[i]=static_cast<double>(i);
+        std::vector<double> diag(n);
+        for (int i = 0; i < n; i++)
+            diag[i] = static_cast<double>(i);
 
-    ReplicatedMatrix mat("diag", diag.data(), n, n);
-    ReplicatedMatrix evects("evects", n, n);
-    std::vector<double> evals(n);
-    mat.syev('T', 'U', evals, evects);
+        ReplicatedMatrix mat("diag", diag.data(), n, n);
+        ReplicatedMatrix evects("evects", n, n);
+        std::vector<double> evals(n);
+        mat.syev('T', 'U', evals, evects);
 
-    for(int i=0;i<n;i++)CHECK(evals[i] == Approx(diag[i]).epsilon(1.e-8));
+        for (int i = 0; i < n; i++)
+            CHECK(evals[i] == Approx(diag[i]).epsilon(1.e-8));
 
-    double tr = mat.trace();
-    CHECK(tr == Approx(static_cast<double>(n*(n-1)/2)).epsilon(1.e-8));
+        double tr = mat.trace();
+        CHECK(
+            tr == Approx(static_cast<double>(n * (n - 1) / 2)).epsilon(1.e-8));
     }
 
     // set values using SquareLocalMatrices
     // then consolidate matrix
     {
-    double value=0.1;
-    SquareLocalMatrices<double> slm(1,n);
-    for(int i=0;i<n;i++)
-    for(int j=0;j<n;j++)slm.setVal(i,j,value);
+        double value = 0.1;
+        SquareLocalMatrices<double> slm(1, n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                slm.setVal(i, j, value);
 
-    ReplicatedMatrix mat("mat", n, n);
-    mat.assign(slm);
-    mat.consolidate();
-    std::vector<double> values(n*n);
-    mat.get(values.data(),n);
-    for(int i=0;i<n*n;i++)CHECK(values[i] == Approx(value*static_cast<double>(npes)).epsilon(1.e-8));
-
+        ReplicatedMatrix mat("mat", n, n);
+        mat.assign(slm);
+        mat.consolidate();
+        std::vector<double> values(n * n);
+        mat.get(values.data(), n);
+        for (int i = 0; i < n * n; i++)
+            CHECK(values[i]
+                  == Approx(value * static_cast<double>(npes)).epsilon(1.e-8));
     }
 }
