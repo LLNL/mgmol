cf1e2e1d853a9d3e890abe9d7d14995ec24e8529
Jean-Luc Fattebert <fattebertj@ornl.gov>
2020-10-01 12:44:04 -0400

    Fix format

--- src/ReplicatedMatrix.cc
+++ src/ReplicatedMatrix.cc
@@ -23,7 +23,7 @@ using MemoryDev = MemorySpace::Memory<double, MemorySpace::Device>;
 constexpr double gpuroundup = 32;
 
 MPI_Comm ReplicatedMatrix::comm_ = MPI_COMM_NULL;
-bool ReplicatedMatrix::onpe0_ = false;
+bool ReplicatedMatrix::onpe0_    = false;
 
 void rotateSym(ReplicatedMatrix& mat, const ReplicatedMatrix& rotation_matrix,
     ReplicatedMatrix& work)
@@ -149,7 +149,8 @@ void ReplicatedMatrix::add(const SquareSubMatrix<double>& mat)
         }
     }
 
-    std::unique_ptr<double, void (*)(double*)> src_dev(MemoryDev::allocate(dim_ * ld_), MemoryDev::free);
+    std::unique_ptr<double, void (*)(double*)> src_dev(
+        MemoryDev::allocate(dim_ * ld_), MemoryDev::free);
 
     auto& magma_singleton = MagmaSingleton::get_magma_singleton();
 
@@ -158,8 +159,8 @@ void ReplicatedMatrix::add(const SquareSubMatrix<double>& mat)
         magma_singleton.queue_);
 
     // add to object data
-    magmablas_dgeadd(dim_, dim_, 1., src_dev.get(), ld_,
-        device_data_.get(), ld_, magma_singleton.queue_);
+    magmablas_dgeadd(dim_, dim_, 1., src_dev.get(), ld_, device_data_.get(),
+        ld_, magma_singleton.queue_);
 }
 
 void ReplicatedMatrix::init(const double* const ha, const int lda)
@@ -449,11 +450,11 @@ double ReplicatedMatrix::trace() const
     auto& magma_singleton = MagmaSingleton::get_magma_singleton();
 
     // this is a little contorted, but it works for now...
-    std::unique_ptr<double, void (*)(double*)> 
-        tmp_dev(MemoryDev::allocate(dim_ * ld_), MemoryDev::free);
+    std::unique_ptr<double, void (*)(double*)> tmp_dev(
+        MemoryDev::allocate(dim_ * ld_), MemoryDev::free);
     const std::vector<double> val(dim_, 1.);
-    magma_dsetvector(dim_, val.data(), 1, tmp_dev.get(), 1,
-        magma_singleton.queue_);
+    magma_dsetvector(
+        dim_, val.data(), 1, tmp_dev.get(), 1, magma_singleton.queue_);
 
     return magma_ddot(dim_, device_data_.get(), ld_ + 1, tmp_dev.get(), 1,
         magma_singleton.queue_);
@@ -537,13 +538,13 @@ void ReplicatedMatrix::print(std::ostream& os, const int ia, const int ja,
     magma_dgetmatrix(dim_, dim_, device_data_.get(), ld_, mat.data(), dim_,
         magma_singleton.queue_);
 
-    if(onpe0_)
-    for (int i = ia; i < m; i++)
-    {
-        for (int j = ja; j < n; j++)
-            os << mat[i + j * dim_] << "   ";
-        os << std::endl;
-    }
+    if (onpe0_)
+        for (int i = ia; i < m; i++)
+        {
+            for (int j = ja; j < n; j++)
+                os << mat[i + j * dim_] << "   ";
+            os << std::endl;
+        }
 }
 
 void ReplicatedMatrix::printMM(std::ostream& os) const {}
