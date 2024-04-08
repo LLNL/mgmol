7ddb74c9701907d398571a47e30851894ec0f336
Kevin Chung <dreamer2368@gmail.com>
2024-04-08 12:52:59 -0700

    changed the Signal.h header file name.

--- src/md.cc
+++ src/md.cc
@@ -31,7 +31,7 @@
 #include "ProjectedMatricesMehrstellen.h"
 #include "ProjectedMatricesSparse.h"
 #include "Rho.h"
-#include "Signal.h"
+#include "Signal_mgmol.h"
 #include "SpreadsAndCenters.h"
 #include "tools.h"
 
