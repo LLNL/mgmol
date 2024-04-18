482353cda2f799fb93efb719f2c167979e44511d
Kevin Chung <dreamer2368@gmail.com>
2024-04-17 17:40:43 -0700

    separated option description routines.

--- src/rom_main.cc
+++ src/rom_main.cc
@@ -58,8 +58,7 @@ using namespace std;
 #include <sys/cdefs.h>
 #include <time.h>
 
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
+#include "OptionDescription.h"
 
 #include "librom.h"
 
@@ -82,7 +81,132 @@ int main(int argc, char** argv)
     assert(mype > -1);
     onpe0 = (mype == 0);
 
-    CAROM::Vector librom_vector(10, false);
+    string input_file("");
+    string lrs_filename;
+    string constraints_filename("");
+    bool tcheck = false;
+
+    float total_spin = 0.;
+    bool with_spin   = false;
+
+    po::variables_map vm;
+
+    // use configure file if it can be found
+    // std::string config_filename("mgmol.cfg");
+
+    // read options from PE0 only
+    if (onpe0) try
+        {
+            string config_file;
+
+            // Declare a group of options that will be
+            // allowed only on command line
+            po::options_description generic("Generic options");
+            setupGenericOption(generic, config_file, lrs_filename);
+
+            // Declare a group of options (with default when appropriate) that
+            // will be allowed in config file
+            po::options_description config("Configuration");
+            setupConfigOption(config, constraints_filename);
+
+            // Hidden options, will be allowed in config file, but will not be
+            // shown to the user.
+            po::options_description hidden("Hidden options");
+            setupHiddenOption(hidden);
+
+            po::options_description cmdline_options;
+            cmdline_options.add(generic);
+
+            po::options_description config_file_options;
+            config_file_options.add(config).add(hidden);
+
+            po::options_description visible("Allowed options");
+            visible.add(generic).add(config);
+
+            po::positional_options_description pd;
+            pd.add("atomicCoordinates", -1);
+
+            store(po::command_line_parser(argc, argv)
+                      .options(cmdline_options)
+                      .positional(pd)
+                      .run(),
+                vm);
+            notify(vm);
+
+            ifstream ifs(config_file.c_str());
+            if (!ifs)
+            {
+                cout << "can not open config file: " << config_file << "\n";
+                return 0;
+            }
+            else
+            {
+                store(parse_config_file(ifs, config_file_options), vm);
+                notify(vm);
+            }
+
+            if (vm.count("help"))
+            {
+                if (onpe0) cout << visible << "\n";
+                return 0;
+            }
+
+            if (vm.count("version"))
+            {
+                if (onpe0)
+                {
+#ifdef GITHASH
+#define xstr(x) #x
+#define LOG(x) cout << " MGmol: git_hash " << xstr(x) << endl;
+                    LOG(GITHASH);
+                    cout << endl;
+#endif
+                }
+                return 0;
+            }
+
+            if (vm.count("check"))
+            {
+                tcheck = true;
+            }
+            if (vm.count("spin"))
+            {
+                total_spin = vm["spin"].as<float>();
+                with_spin  = true;
+                if (onpe0) cout << "Spin was set to " << total_spin << endl;
+            }
+            if (vm.count("atomicCoordinates"))
+            {
+                if (onpe0)
+                    cout << "Input files is: "
+                         << vm["atomicCoordinates"].as<vector<string>>()[0]
+                         << "\n";
+                input_file = vm["atomicCoordinates"].as<vector<string>>()[0];
+            }
+            else
+            {
+                if (vm["Restart.input_level"].as<short>() == 0)
+                {
+                    throw std::runtime_error(
+                        "ERROR: No coordinates file provided!!!");
+                }
+            }
+
+            if (onpe0) cout << "Spin: " << total_spin << "\n";
+
+        } // try
+        catch (exception& e)
+        {
+            cerr << e.what() << "\n";
+            return 1;
+        }
+
+    MGmol_MPI::setup(MPI_COMM_WORLD, std::cout, with_spin);
+    MGmol_MPI& mmpi      = *(MGmol_MPI::instance());
+    MPI_Comm global_comm = mmpi.commGlobal();
+
+    Control::setup(global_comm, with_spin, total_spin);
+    Control& ct = *(Control::instance());
 
     mpirc = MPI_Finalize();
     if (mpirc != MPI_SUCCESS)
