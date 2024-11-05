a733973bc917d0fd02a6d0d56dee9179e039374f
Siu Wun Cheung <cheung26@llnl.gov>
2024-11-05 10:14:51 -0800

    Add new files

---
+++ examples/PinnedH2O/generate_coord_simple.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+
+O1 = np.array([0.00, 0.00, 0.00])
+
+ref_bondlength = 1.83
+ref_bondangle = 104.5
+
+# factors and increments for bond lengths and bond angle
+bondlength1_factor = np.linspace(0.95, 1.05, 11)
+bondlength2_factor = np.linspace(0.95, 1.05, 11)
+bondangle_increment = np.linspace(-5, 5, 11)
+
+# output directory
+output_dir = "PinnedH2O_3dof_coords"
+
+# generation
+os.makedirs(output_dir, exist_ok=True)
+
+for d_bondangle in bondangle_increment:
+    bondangle = ref_bondangle + d_bondangle
+    x = ref_bondlength * np.cos(np.radians(bondangle / 2))
+    y = ref_bondlength * np.cos(np.radians(bondangle / 2))
+    for f_bondlength1 in bondlength1_factor:
+        for f_bondlength2 in bondlength2_factor:
+            H1 = np.array([f_bondlength1*x, f_bondlength1*y, 0.0])
+            H2 = np.array([f_bondlength2*x, -f_bondlength2*y, 0.0])
+            filename = f"{output_dir}/coords_{f_bondlength1:.2f}_{f_bondlength2:.2f}_{d_bondangle}.in"
+            with open(filename, "w") as file:
+                file.write(f"O1  1   {O1[0]:.2f}    {O1[1]:.2f}    {O1[2]:.2f}  0\n")
+                file.write(f"H1  2   {H1[0]:.2f}    {H1[1]:.2f}    {H1[2]:.2f}  1\n")
+                file.write(f"H2  2   {H2[0]:.2f}    {H2[1]:.2f}    {H2[2]:.2f}  1\n")
