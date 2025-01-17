0777a9543088c416c868c089535be20faef84939
Siu Wun Cheung <cheung26@llnl.gov>
2025-01-17 14:12:06 -0800

    Add

--- tests/PinnedH2O_3DOF/testPinnedH2O_3DOF.cc
+++ tests/PinnedH2O_3DOF/testPinnedH2O_3DOF.cc
@@ -173,6 +173,14 @@ int main(int argc, char** argv)
         orbitals.set(ct.getROMOptions().basis_file, ct.getROMOptions().num_orbbasis); 
         orbitals.computeGramAndInvS();
 
+        // set the iterative index to 1 to differentiate it from first instance
+        // in MGmol initial() function. This is not very clean and could be
+        // better designed, but works for now
+        orbitals.setIterativeIndex(1);
+
+        // set initial DM with uniform occupations
+        projmatrices->setDMuniform(ct.getNelSpin(), 0);
+
         //
         // evaluate energy and forces again
         //
