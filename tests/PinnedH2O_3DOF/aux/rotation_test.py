f6fd1218d694f98d243c4d0ce29ae4311b84d067
Siu Wun Cheung <cheung26@llnl.gov>
2025-02-10 13:56:45 -0800

    Fix calculations

--- tests/PinnedH2O_3DOF/aux/rotation_test.py
+++ tests/PinnedH2O_3DOF/aux/rotation_test.py
@@ -17,25 +17,6 @@ def calculate_bondangle(atom1, atom2, atom3, radian):
         angle = np.degrees(angle)
     return angle
 
-def rotation_matrix(axis, angle):
-    cos_theta = np.cos(angle)
-    sin_theta = np.sin(angle)
-    ux, uy, uz = axis
-    return np.array([
-        [cos_theta + ux**2 * (1 - cos_theta), ux * uy * (1 - cos_theta) - uz * sin_theta, ux * uz * (1 - cos_theta) + uy * sin_theta],
-        [uy * ux * (1 - cos_theta) + uz * sin_theta, cos_theta + uy**2 * (1 - cos_theta), uy * uz * (1 - cos_theta) - ux * sin_theta],
-        [uz * ux * (1 - cos_theta) - uy * sin_theta, uz * uy * (1 - cos_theta) + ux * sin_theta, cos_theta + uz**2 * (1 - cos_theta)]
-    ])
-
-plane_normal = np.cross(H2, H1)
-plane_normal = plane_normal / np.linalg.norm(plane_normal)
-
-target_plane_normal = np.array([0, 0, 1])
-axis_to_align = np.cross(plane_normal, target_plane_normal)
-axis_to_align /= np.linalg.norm(axis_to_align)
-angle_to_align = np.arccos(np.clip(np.dot(plane_normal, target_plane_normal), -1.0, 1.0))
-rot_matrix_align_plane = rotation_matrix(axis_to_align, angle_to_align)
-
 bondlength1 = calculate_bondlength(H1, O1)
 bondlength2 = calculate_bondlength(H2, O1)
 bondangle = calculate_bondangle(H1, O1, H2, False)
@@ -47,34 +28,72 @@ print(f'Bondlength of O1-H1 = {bondlength1}')
 print(f'Bondlength of O1-H2 = {bondlength2}')
 print(f'Angle between O1-H1 and O1-H2 = {bondangle}')
 
+def rotation_matrix(axis, angle):
+    cos_theta = np.cos(angle)
+    sin_theta = np.sin(angle)
+    ux, uy, uz = axis
+    return np.array([
+        [cos_theta + ux**2 * (1 - cos_theta), ux * uy * (1 - cos_theta) - uz * sin_theta, ux * uz * (1 - cos_theta) + uy * sin_theta],
+        [uy * ux * (1 - cos_theta) + uz * sin_theta, cos_theta + uy**2 * (1 - cos_theta), uy * uz * (1 - cos_theta) - ux * sin_theta],
+        [uz * ux * (1 - cos_theta) - uy * sin_theta, uz * uy * (1 - cos_theta) + ux * sin_theta, cos_theta + uz**2 * (1 - cos_theta)]
+    ])
+
+H1_rotated, H2_rotated = H1, H2
+
+plane_normal = np.cross(H2, H1)
+plane_normal = plane_normal / np.linalg.norm(plane_normal)
+
+target_plane_normal = np.array([0, 0, 1])
+axis_to_align = np.cross(plane_normal, target_plane_normal)
+axis_to_align /= np.linalg.norm(axis_to_align)
+angle_to_align = np.arccos(np.clip(np.dot(plane_normal, target_plane_normal), -1.0, 1.0))
+rot_matrix_align_plane = rotation_matrix(axis_to_align, angle_to_align)
 H1_rotated = np.dot(rot_matrix_align_plane, H1)
 H2_rotated = np.dot(rot_matrix_align_plane, H2)
-fliped_bond = False
-if bondlength1 < bondlength2:
-    fliped_bond = True
-    H1_rotated, H2_rotated = H2_rotated, H1_rotated
-bondlength1 = calculate_bondlength(H1_rotated, O1)
-bondlength2 = calculate_bondlength(H2_rotated, O1)
-bondangle = calculate_bondangle(H1_rotated, O1, H2_rotated, False)
 
-print('Reference system (z=0 plane about x=0 axis, with longer bondlength in H1)')
+theta1 = np.arctan2(H1_rotated[1], H1_rotated[0]) 
+theta2 = np.arctan2(H2_rotated[1], H2_rotated[0])
+planar_rotation_angle = -theta1 + np.radians(bondangle) / 2 
+planar_rotation_matrix = rotation_matrix(target_plane_normal, planar_rotation_angle)
+H1_rotated = np.dot(planar_rotation_matrix, H1_rotated)
+H2_rotated = np.dot(planar_rotation_matrix, H2_rotated)
+
+if bondlength1 < bondlength2:
+    H1_rotated, H2_rotated = H2_rotated, H1_rotated 
+    H1_rotated[1] *= -1
+    H2_rotated[1] *= -1
+
+bondlength1_rotated = calculate_bondlength(H1_rotated, O1)
+bondlength2_rotated = calculate_bondlength(H2_rotated, O1)
+bondangle_rotated = calculate_bondangle(H1_rotated, O1, H2_rotated, False)
+
+print('Reference system (z=0 plane, symmetric about x=0 axis, with longer bondlength in Q1)')
 print(f'H1 = {H1_rotated}')
 print(f'H2 = {H2_rotated}')
-print(f'Bondlength of O1-H1 = {bondlength1}')
-print(f'Bondlength of O1-H2 = {bondlength2}')
-print(f'Angle between O1-H1 and O1-H2 = {bondangle}')
+print(f'Bondlength of O1-H1 = {bondlength1_rotated}')
+print(f'Bondlength of O1-H2 = {bondlength2_rotated}')
+print(f'Angle between O1-H1 and O1-H2 = {bondangle_rotated}')
 
-if fliped_bond:
-    H1_rotated, H2_rotated = H2_rotated, H1_rotated
-H1_restored = np.dot(rot_matrix_align_plane.T, H1_rotated)
-H2_restored = np.dot(rot_matrix_align_plane.T, H2_rotated)
-bondlength1 = calculate_bondlength(H1_restored, O1)
-bondlength2 = calculate_bondlength(H2_restored, O1)
-bondangle = calculate_bondangle(H1_restored, O1, H2_restored, False)
+H1_restored, H2_restored = H1_rotated, H2_rotated
+
+if bondlength1 < bondlength2:
+    H1_rotated, H2_rotated = H2_rotated, H1_rotated 
+    H1_rotated[1] *= -1
+    H2_rotated[1] *= -1
+
+H1_restored = np.dot(planar_rotation_matrix.T, H1_restored)
+H2_restored = np.dot(planar_rotation_matrix.T, H2_restored)
+
+H1_restored = np.dot(rot_matrix_align_plane.T, H1_restored)
+H2_restored = np.dot(rot_matrix_align_plane.T, H2_restored)
+
+bondlength1_restored = calculate_bondlength(H1_restored, O1)
+bondlength2_restored = calculate_bondlength(H2_restored, O1)
+bondangle_restored = calculate_bondangle(H1_restored, O1, H2_restored, False)
 
 print('Restored system')
 print(f'H1 = {H1_restored}')
 print(f'H2 = {H2_restored}')
-print(f'Bondlength of O1-H1 = {bondlength1}')
-print(f'Bondlength of O1-H2 = {bondlength2}')
-print(f'Angle between O1-H1 and O1-H2 = {bondangle}')
+print(f'Bondlength of O1-H1 = {bondlength1_restored}')
+print(f'Bondlength of O1-H2 = {bondlength2_restored}')
+print(f'Angle between O1-H1 and O1-H2 = {bondangle_restored}')
