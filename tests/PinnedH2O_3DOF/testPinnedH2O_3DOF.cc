1d3a2026fd448a8b4d4f9ab01a1a5fc1564d8882
Siu Wun Cheung <cheung26@llnl.gov>
2025-02-20 13:53:36 -0800

    Move geometric transforms to tools in library

--- tests/PinnedH2O_3DOF/testPinnedH2O_3DOF.cc
+++ tests/PinnedH2O_3DOF/testPinnedH2O_3DOF.cc
@@ -14,6 +14,7 @@
 #include "MGmol_MPI.h"
 #include "MPIdata.h"
 #include "mgmol_run.h"
+#include "PinnedH2O.h"
 
 #ifdef MGMOL_HAS_LIBROM
 #include "librom.h"
@@ -28,215 +29,6 @@
 
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
-
-double calculate_bondlength(const double atom1[3], const double atom2[3])
-{
-    return sqrt(pow(atom1[0] - atom2[0], 2) + pow(atom1[1] - atom2[1], 2) + pow(atom1[2] - atom2[2], 2));
-}
-
-double calculate_bondangle(const double atom1[3], const double atom2[3], const double atom3[3])
-{
-    double vector1[3] = {atom1[0] - atom2[0], atom1[1] - atom2[1], atom1[2] - atom2[2]};
-    double vector2[3] = {atom3[0] - atom2[0], atom3[1] - atom2[1], atom3[2] - atom2[2]};
-
-    double dot_product = vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2];
-    double magnitude_product = sqrt(pow(vector1[0], 2) + pow(vector1[1], 2) + pow(vector1[2], 2)) *
-                               sqrt(pow(vector2[0], 2) + pow(vector2[1], 2) + pow(vector2[2], 2));
-    double angle = acos(dot_product / magnitude_product);
-
-    return angle;
-}
-
-void rotation_matrix(const double axis[3], double angle, double matrix[3][3])
-{
-    double cos_theta = cos(angle);
-    double sin_theta = sin(angle);
-    double ux = axis[0], uy = axis[1], uz = axis[2];
-
-    matrix[0][0] = cos_theta + ux * ux * (1 - cos_theta);
-    matrix[0][1] = ux * uy * (1 - cos_theta) - uz * sin_theta;
-    matrix[0][2] = ux * uz * (1 - cos_theta) + uy * sin_theta;
-
-    matrix[1][0] = uy * ux * (1 - cos_theta) + uz * sin_theta;
-    matrix[1][1] = cos_theta + uy * uy * (1 - cos_theta);
-    matrix[1][2] = uy * uz * (1 - cos_theta) - ux * sin_theta;
-
-    matrix[2][0] = uz * ux * (1 - cos_theta) - uy * sin_theta;
-    matrix[2][1] = uz * uy * (1 - cos_theta) + ux * sin_theta;
-    matrix[2][2] = cos_theta + uz * uz * (1 - cos_theta);
-}
-
-void normalize(double vec[3])
-{
-    double norm = sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
-    vec[0] /= norm;
-    vec[1] /= norm;
-    vec[2] /= norm;
-}
-
-void cross(const double a[3], const double b[3], double result[3])
-{
-    result[0] = a[1] * b[2] - a[2] * b[1];
-    result[1] = a[2] * b[0] - a[0] * b[2];
-    result[2] = a[0] * b[1] - a[1] * b[0];
-}
-
-void apply_rotation(const double matrix[3][3], const double vec[3], double result[3])
-{
-    result[0] = matrix[0][0] * vec[0] + matrix[0][1] * vec[1] + matrix[0][2] * vec[2];
-    result[1] = matrix[1][0] * vec[0] + matrix[1][1] * vec[1] + matrix[1][2] * vec[2];
-    result[2] = matrix[2][0] * vec[0] + matrix[2][1] * vec[1] + matrix[2][2] * vec[2];
-}
-
-void apply_transpose_rotation(const double matrix[3][3], const double vec[3], double result[3])
-{
-    result[0] = matrix[0][0] * vec[0] + matrix[1][0] * vec[1] + matrix[2][0] * vec[2];
-    result[1] = matrix[0][1] * vec[0] + matrix[1][1] * vec[1] + matrix[2][1] * vec[2];
-    result[2] = matrix[0][2] * vec[0] + matrix[1][2] * vec[1] + matrix[2][2] * vec[2];
-}
-
-void rotate_PinnedH2O(std::vector<double>& positions, std::vector<short>& anumbers,
-                      double out_of_plane_rotation_matrix[3][3], double& planar_rotation_angle, bool& flipped_bond)
-{
-    int O1_idx = -1;
-    for (int i = 0; i < 3; i++)
-    {
-        if (positions[3*i] == 0.0 && positions[3*i+1] == 0.0 && positions[3*i+2] == 0.0)
-        {
-            O1_idx = i;
-            break;
-        }
-    }
-    if (O1_idx == -1) return;
-
-    int H1_idx = (O1_idx + 1) % 3;
-    int H2_idx = (O1_idx + 2) % 3;
-
-    double O1[3] = {positions[3*O1_idx], positions[3*O1_idx+1], positions[3*O1_idx+2]};
-    double H1[3] = {positions[3*H1_idx], positions[3*H1_idx+1], positions[3*H1_idx+2]};
-    double H2[3] = {positions[3*H2_idx], positions[3*H2_idx+1], positions[3*H2_idx+2]};
-
-    double bondlength1 = calculate_bondlength(H1, O1);
-    double bondlength2 = calculate_bondlength(H2, O1);
-    double bondangle = calculate_bondangle(H1, O1, H2);
-
-    double H1_temp[3], H2_temp[3];
-    double H1_rotated[3], H2_rotated[3];
-
-    double plane_normal[3];
-    cross(H2, H1, plane_normal);
-    normalize(plane_normal);
-    double target_plane_normal[3] = {0, 0, 1};
-    double dot_product = plane_normal[0] * target_plane_normal[0] +
-                         plane_normal[1] * target_plane_normal[1] +
-                         plane_normal[2] * target_plane_normal[2];
-    double angle_to_align = std::acos(std::min(std::max(dot_product, -1.0), 1.0));
-    double axis_to_align[3];
-    if (abs(dot_product) > 1.0 - 1e-8)
-    {
-        axis_to_align[0] = 1.0; 
-        axis_to_align[1] = 0.0;
-        axis_to_align[2] = 0.0;
-    }
-    else
-    {
-        cross(plane_normal, target_plane_normal, axis_to_align);
-        normalize(axis_to_align);
-    }
-    rotation_matrix(axis_to_align, angle_to_align, out_of_plane_rotation_matrix);
-    apply_rotation(out_of_plane_rotation_matrix, H1, H1_temp);
-    apply_rotation(out_of_plane_rotation_matrix, H2, H2_temp);
-
-    double theta1 = std::atan2(H1_temp[1], H1_temp[0]);
-    planar_rotation_angle = -theta1 + bondangle / 2.0;
-    double planar_rotation_matrix[3][3];
-    rotation_matrix(target_plane_normal, planar_rotation_angle, planar_rotation_matrix);
-    apply_rotation(planar_rotation_matrix, H1_temp, H1_rotated);
-    apply_rotation(planar_rotation_matrix, H2_temp, H2_rotated);
-
-    flipped_bond = (bondlength1 < bondlength2);
-    if (flipped_bond)
-    {
-        H1_rotated[1] *= -1.0;
-        H2_rotated[1] *= -1.0;
-        std::swap(H1_rotated, H2_rotated);
-    }
-
-    positions[0] = H2_rotated[0];
-    positions[1] = H2_rotated[1];
-    positions[2] = H2_rotated[2];
-    positions[3] = 0.0;
-    positions[4] = 0.0;
-    positions[5] = 0.0;
-    positions[6] = H1_rotated[0];
-    positions[7] = H1_rotated[1];
-    positions[8] = H1_rotated[2];
-
-    anumbers[0] = 1;
-    anumbers[1] = 8;
-    anumbers[2] = 1;
-}
-
-void transpose_rotate_PinnedH2O(std::vector<double>& positions, std::vector<double>& forces,
-                                const double out_of_plane_rotation_matrix[3][3], 
-                                const double planar_rotation_angle, const bool flipped_bond)
-{
-    double H2_rotated[3] = {positions[0], positions[1], positions[2]};
-    double O1_rotated[3] = {positions[3], positions[4], positions[5]};
-    double H1_rotated[3] = {positions[6], positions[7], positions[8]};
-
-    double f_H2_rotated[3] = {forces[0], forces[1], forces[2]};
-    double f_O1_rotated[3] = {forces[3], forces[4], forces[5]};
-    double f_H1_rotated[3] = {forces[6], forces[7], forces[8]};
-
-    if (flipped_bond)
-    {
-        H1_rotated[1] *= -1.0;
-        H2_rotated[1] *= -1.0;
-        f_O1_rotated[1] *= -1.0;
-        f_H1_rotated[1] *= -1.0;
-        f_H2_rotated[1] *= -1.0;
-    }
-
-    double planar_rotation_matrix[3][3];
-    double target_plane_normal[3] = {0, 0, 1};
-    rotation_matrix(target_plane_normal, planar_rotation_angle, planar_rotation_matrix);
-
-    double H1_temp[3], H2_temp[3];
-    apply_transpose_rotation(planar_rotation_matrix, H1_rotated, H1_temp);
-    apply_transpose_rotation(planar_rotation_matrix, H2_rotated, H2_temp);
-
-    double f_O1_temp[3], f_H1_temp[3], f_H2_temp[3];
-    apply_transpose_rotation(planar_rotation_matrix, f_O1_rotated, f_O1_temp);
-    apply_transpose_rotation(planar_rotation_matrix, f_H1_rotated, f_H1_temp);
-    apply_transpose_rotation(planar_rotation_matrix, f_H2_rotated, f_H2_temp);
-
-    double H1_restored[3], H2_restored[3];
-    apply_transpose_rotation(out_of_plane_rotation_matrix, H1_temp, H1_restored);
-    apply_transpose_rotation(out_of_plane_rotation_matrix, H2_temp, H2_restored);
-
-    double f_O1_restored[3], f_H1_restored[3], f_H2_restored[3];
-    apply_transpose_rotation(out_of_plane_rotation_matrix, f_O1_temp, f_O1_restored);
-    apply_transpose_rotation(out_of_plane_rotation_matrix, f_H1_temp, f_H1_restored);
-    apply_transpose_rotation(out_of_plane_rotation_matrix, f_H2_temp, f_H2_restored);
-
-    positions[0] = H2_restored[0];
-    positions[1] = H2_restored[1];
-    positions[2] = H2_restored[2];
-    positions[6] = H1_restored[0];
-    positions[7] = H1_restored[1];
-    positions[8] = H1_restored[2];
-
-    forces[0] = f_H2_restored[0];
-    forces[1] = f_H2_restored[1];
-    forces[2] = f_H2_restored[2];
-    forces[3] = f_O1_restored[0];
-    forces[4] = f_O1_restored[1];
-    forces[5] = f_O1_restored[2];
-    forces[6] = f_H1_restored[0];
-    forces[7] = f_H1_restored[1];
-    forces[8] = f_H1_restored[2];
-}
 
 int main(int argc, char** argv)
 {
@@ -340,11 +132,8 @@ int main(int argc, char** argv)
         }
 
         // rotate the molecule to the reference coordinate system
-        int atom_order[3];
-        double out_of_plane_rotation_matrix[3][3];
-        double planar_rotation_angle;
-        bool flipped_bond;
-        rotate_PinnedH2O(positions, anumbers, out_of_plane_rotation_matrix, planar_rotation_angle, flipped_bond);
+        PinnedH2O H2O_molecule;
+        H2O_molecule.rotate(positions, anumbers);
 
         Mesh* mymesh             = Mesh::instance();
         const pb::Grid& mygrid   = mymesh->grid();
@@ -417,7 +206,7 @@ int main(int argc, char** argv)
         }
 
         // rotate the forces to the original coordinate system
-        transpose_rotate_PinnedH2O(positions, forces, out_of_plane_rotation_matrix, planar_rotation_angle, flipped_bond);
+        H2O_molecule.transpose_rotate(positions, forces);
 
         // print out results
         if (MPIdata::onpe0)
