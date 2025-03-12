964139c521d17aa6a60644b42af1e62dd2e424f4
Siu Wun Cheung <cheung26@llnl.gov>
2025-03-12 14:59:56 -0700

    Add initialization to evaluateDMandEnergyandForces if it is ROM MVP

--- src/MGmol.cc
+++ src/MGmol.cc
@@ -1510,6 +1510,13 @@ double MGmol<OrbitalsType>::evaluateDMandEnergyAndForces(Orbitals* orbitals,
 
     ions_->setPositions(tau, atnumbers);
 
+#ifdef MGMOL_HAS_LIBROM
+    Control& ct = *(Control::instance());
+    Potentials& pot = hamiltonian_->potential();
+    pot.initialize(*ions_);
+    g_kbpsi_->setup(*ions_);
+#endif
+
     moveVnuc(*ions_);
 
     // initialize electronic density
