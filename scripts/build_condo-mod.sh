089787685b10c5261d43212f90c2accd0b243f86
Jean-Luc Fattebert <fattebertj@ornl.gov>
2025-07-14 13:38:48 -0400

    Adapt to new environment on condo @ ORNL (#348)

--- scripts/build_condo-mod.sh
+++ scripts/build_condo-mod.sh
@@ -3,9 +3,6 @@
 #Before compiling, load the following modules:
 source scripts/modules.condo-mod
 
-# We need to define the cmake blas vendor option here to find the right one.
-BLAS_VENDOR=OpenBLAS
-
 MGMOL_ROOT=`pwd`
 
 INSTALL_DIR=${MGMOL_ROOT}/install
@@ -15,16 +12,17 @@ BUILD_DIR=${MGMOL_ROOT}/build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
+SCALAPACK_DIR=/home/q8j/Software/ScaLapack/scalapack-2.2.2
+
 # call cmake 
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_COMPILER=mpiCC \
       -DCMAKE_Fortran_COMPILER=mpif77 \
-      -DBLA_VENDOR=${BLAS_VENDOR} \
       -DMGMOL_USE_HDF5P=OFF \
       -DMGMOL_WITH_CLANG_FORMAT=ON \
       -DCMAKE_PREFIX_PATH=${HOME}/bin \
-      -DSCALAPACK_LIBRARY="${SCALAPACK_DIR}/lib/libscalapack.a;/lib64/libgfortran.so.3" \
+      -DSCALAPACK_LIBRARY="${SCALAPACK_DIR}/lib/libscalapack.a;/lib64/libgfortran.so.5" \
       -DMPIEXEC_EXECUTABLE=${OPENMPI_DIR}/bin/mpiexec \
       ..
 
