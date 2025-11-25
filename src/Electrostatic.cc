deeae86252cabe6d36a30bf196b4646f084830d8
Jean-Luc Fattebert <fattebertj@ornl.gov>
2025-11-25 13:12:54 -0500

    Fix potential iterative index (#386)
    * was failing in some assert

--- src/Electrostatic.cc
+++ src/Electrostatic.cc
@@ -319,8 +319,7 @@ void Electrostatic::computeVh(const Ions& ions, Rho<T>& rho, Potentials& pot)
         eepsilon_ = 0.;
     }
 
-    iterative_index_ = pot.getIterativeIndex();
-    iterative_index_++;
+    iterative_index_ = rho.getIterativeIndex();
     pot.setVh(poisson_solver_->vh(), iterative_index_);
 
     if (diel_flag_)
